f34bb9cc767adff692d7ac7701c2ac1a9b04cc51
tuanld <tuanld@bravestars.com>
2021-05-02 19:07:07 +0700

    update client

---
+++ test.js
@@ -0,0 +1,27 @@
+const { MongoClient, ObjectID } = require('mongodb');
+const config = require('./config');
+
+var test = async function() {
+    const client = new MongoClient(config.db.uri, { useUnifiedTopology: true });
+    let obStoreDataGame = {
+        "5f552458db096a3ebd469155":[],//tlmn1
+        "5f714415630b9b9ff8146f15":[],//tlmn2
+        "5f714415630b9b9ff8146f17":[]
+    }
+    try {
+        await client.connect();
+        const database = client.db('leaderboards');
+        const collection = database.collection(idCollection);
+        const leaderboardId = new ObjectID(req.params.id);
+        // console.log('req.query.friendList   ',req.query.friendList);
+        let friendList = (req.query.friendList)? req.query.friendList.split(',') : null;
+        let records = await collection.find({ lbid: leaderboardId }).toArray();
+        console.log(records.length);
+        res.json({ err: 0, msg: 'ok', entries: records });
+    } catch (e) {
+        res.json({ err: 1, msg: errorFormat(e) });
+    } finally {
+        client.close();
+    }
+}
+test();
