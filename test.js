8a42bfbe31ce1fc4208106185e3303c3cae8e5e8
tuanld <tuanld@bravestars.com>
2021-05-02 19:29:43 +0700

    update client

--- test.js
+++ test.js
@@ -30,7 +30,14 @@ var test = async function() {
         console.log('5f552458db096a3ebd469155    ',obStoreDataGame['5f552458db096a3ebd469155'].length);
         console.log('5f714415630b9b9ff8146f15    ',obStoreDataGame['5f714415630b9b9ff8146f15'].length);
         console.log('5f714415630b9b9ff8146f17    ',obStoreDataGame['5f714415630b9b9ff8146f17'].length);
-        
+        const fs = require('fs');
+        fs.writeFile("UserData", JSON.stringify(obStoreDataGame), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        }); 
+
         // res.json({ err: 0, msg: 'ok', entries: records });
     } catch (e) {
 
@@ -39,5 +46,47 @@ var test = async function() {
     } finally {
         client.close();
     }
+
+    
+    // setTimeout(()=>{
+    //     console.log('Start Clonet ');
+    //     let time = 0;
+    //     for (let key in obStoreDataGame) {
+    //        setTimeout(()=>{
+    //             console.log('start clone ',key);
+    //             let array = obStoreDataGame[key];
+    //             pushCopyUser(array);
+    //        },time);
+    //     }
+        
+    // },5000);
 }
+
+var pushCopyUser = async function(array){
+    let USER = array.shift();
+    const client = new MongoClient(config.db.uri, { useUnifiedTopology: true });
+    let idCollection = `Instant_`+req.params.id;
+    try {
+        await client.connect();
+        const database = client.db('leaderboards');
+        const collection = database.collection(idCollection);
+        const leaderboardId = new ObjectID(req.params.id);
+        const userId = req.body.userId;
+        const score = parseInt(req.body.score);
+        const name = req.body.name;
+        const photo = req.body.photo;
+        await collection.updateOne(
+        // await collection.insertOne(
+        { lbid: USER.lbid, user_id: USER.user_id },
+        { $set: {score: USER.score, username: USER.name, photo:USER.photo, user_id: USER.user_id, updated_at: new Date()} },
+        { upsert: true }
+        );
+        res.json({ err: 0, msg: 'ok' });
+    } catch (e) {
+        res.json({ err: 1, msg: errorFormat(e) });
+    } finally {
+        client.close();
+        pushCopyUser
+    }
+};
 test();
