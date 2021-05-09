42ec9aada5cbae0c402efe71c94ec3656f95f655
tuanld <tuanld@bravestars.com>
2021-05-09 23:33:24 +0700

    update client

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { MongoClient, ObjectID } = require('mongodb');
 const express = require('express');
 const config = require('../config');
 const router = express.Router();
-var cacheDb = {};
+export var cacheDb = {};
 const errorFormat = (e) => {
   return e.message;
 }
@@ -50,7 +50,7 @@ router.post('/:id', async (req, res, next) => {
     const name = req.body.name;
     const photo = req.body.photo;
     if(!cacheDb[req.params.id]) cacheDb[req.params.id] = [];
-    cacheDb[req.params.id].push({
+    let obStore = {
       lbid: leaderboardId,
       user_id: userId,
       score:score,
@@ -58,7 +58,22 @@ router.post('/:id', async (req, res, next) => {
       photo: photo,
       user_id: userId,
       updated_at: new Date()
-    });
+    }
+    if(cacheDb[req.params.id].length == 0){
+      cacheDb[req.params.id]. push(obStore);
+      console.log('1');
+    }else{
+      let user = cacheDb[req.params.id].find(user=>user.user_id == obStore.user_id);
+      if(!user) {
+        cacheDb[req.params.id]. push(obStore);
+        console.log('2');
+      }else{
+        console.log(obStore.user_id,'|',user.score,'|',obStore.score);
+        user.score = obStore.score;
+        console.log(' 3  ',obStore.user_id,' : ',user.score, ' new score ',obStore.score);  
+      }
+    }
+    
     // await collection.updateOne(
     //   { lbid: leaderboardId, user_id: userId },
     //   { $set: {score, username: name, photo, user_id: userId, updated_at: new Date()} },
@@ -90,8 +105,50 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 router.get('/getCacheDb', async (req, res, next) => {
-  let records = cacheDb;
-  res.json({ err: 0, msg: 'ok', entries: records });
- 
+  console.log('cacheDb   ',cacheDb);
+  res.json({ err: 0, msg: 'ok', entries: cacheDb });
+})
+router.get('/saveDatauserToDb',async (req,res,next) =>{
+  var check = async function(id){
+      const client3 = new MongoClient(config.db.uri, { useUnifiedTopology: true }); 
+      try {
+          await client3.connect();
+          const database = client3.db('leaderboards');
+          const collection = database.collection(id);
+          // const leaderboardId = new ObjectID(req.params.id);
+          // console.log('req.query.friendList   ',req.query.friendList);
+          let records = await collection.find({}).toArray();
+          console.log('client3  ',id,' ----->  ',records.length);
+          // res.json({ err: 0, msg: 'ok', entries: records });
+      } catch (e) {
+          console.log('errr ',e);
+          // res.json({ err: 1, msg: errorFormat(e) });
+      } finally {
+          client3.close();
+      }
+  }
+  var pushMany = async function(id,array,callBack){
+      const client = new MongoClient(config.db.uri, { useUnifiedTopology: true });
+      try {
+          await client.connect();
+          const database = client.db('leaderboards');
+          const collection = database.collection(id);
+          await collection.insertMany(array);
+      } catch (e) {
+      } finally {
+          client.close();
+          console.log('DONE USER LB ',id,' =>>> ',array.length);
+          if(callBack) callBack();
+      }
+  }
+  if(cacheDb){
+    for (let key in cacheDb) {
+        let element = object[key];
+        pushMany(key,element,()=>{
+          console.log('DONE   push many  ',key);
+        })
+    }
+  }
+  res.json({ err: 0, msg: 'ok' });
 })
 module.exports = router;
